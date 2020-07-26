bba98d6ad5fde265b0701b50686eae83dff06952
Akash Kalia <akashkalia@protonmail.com>
2020-07-26 13:32:45 +0100

    Scrapes indeed.co.uk for jobs related to specified keyword. Sends either an email or creates xls file.

--- job_scraper.py
+++ job_scraper.py
@@ -1,8 +1,13 @@
-
-import json 
+from datetime import datetime
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 import os 
+from smtplib import SMTP 
 import smtplib
+import ssl 
+import sys
 import urllib
+
 from bs4 import BeautifulSoup
 from decouple import config
 import pandas as pd
@@ -24,30 +29,45 @@ with open(os.path.join(os.path.dirname(BASE_DIR), "secrets.json")) as secrets_fi
     secrets = json.load(secrets_file)
 
 def get_secret(setting, secrets=secrets):
-    """
     #Get secret setting or fail with ImproperlyConfigured.
-    """
+    
     try:
         return secrets[setting]
     except KeyError:
         raise ImproperlyConfigured(f"Set the {setting} setting.")
 
 """
+def search_job_listings(job_title, location, desired_result, output):
+	job_soup = load_indeed_jobs_div(job_title, location)
+	job_list, num_listings = extract_job_information_indeed(job_soup, desired_result)
 
-def load_indeed_jobs_div(job_title, location):
-	args = {'q': job_title, 'l': location, 'fromage':'last', 'sort': 'date'}
-	url = ('http://www.indeed.co.uk/jobs?' + urllib.parse.urlencode(args))
-	page = requests.get(url)
-	soup = BeautifulSoup(page.content, "lxml")
-	jobs = soup.find(id="resultsCol")
-	return jobs
+	if output == 1:
+		send_mail(job_list)
+
+	elif output == 2:
+		filename = input("Please enter a filename\n.\
+			For example 'job_listings'.\n> ").split(".")[0]
+		save_jobs_to_excel(job_list, filename)
+
+
+	print(f"{num_listings} new job postings retrieved from Indeed.")
 
 
 
+## Output options. Email and Excel.
 
 def send_mail(jobs_list):
 	jobs = pd.DataFrame(jobs_list)
 
+	from_email = config("FROM_EMAIL")
+	from_password = config("FROM_PASSWORD")
+	to_email = config("TO_EMAIL")
+	message = MIMEMultipart()
+	#message = EmailMessage()
+	now = datetime.now().strftime("%d/%m/%Y at %H:%M:%S")
+	message['Subject'] = f'Job listings on {now}'
+	message['From'] = from_email
+	message['To'] = to_email
 	html = """\
 	<html>
 		<head></head>
@@ -57,14 +77,26 @@ def send_mail(jobs_list):
 	</html>
 	""".format(jobs.to_html())
 
-	from_email = config(FROM_EMAIL)
-	from_password = config(FROM_PASSWORD)
+	#message.set_content(html)
+	s = SMTP()
 
+	part1 = MIMEText(html, 'html')
+	message.attach(part1)
 
+	# Send email using Gmail
+	context = ssl.create_default_context()
+	with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+		server.login(from_email, from_password)
+		server.sendmail(
+			from_email, to_email, message.as_string()
+		)
+	print(f"Email sent to {to_email}")
 
+def save_jobs_to_excel(jobs_list, filename):
+    jobs = pd.DataFrame(jobs_list)
+    jobs.to_excel(filename)
 
-
-
+## INDEED JOB SEARCH FUNCTIONS 
 
 
 def extract_job_information_indeed(jobs, desired_result):
@@ -73,19 +105,19 @@ def extract_job_information_indeed(jobs, desired_result):
 	cols = []
 	extracted_info = []
 
-	if 'titles' inn desired_result:
+	if 'titles' in desired_result:
 		titles = []
 		cols.append('titles')
 		for element in job_elements:
 			titles.append(extract_job_title_indeed(element))
-		extracted_info.append(titles)
+		extracted_info.append(titles) 
 
 	if 'companies' in desired_result:
-		companies = []
+		companies = [] 
 		cols.append('companies')
 		for element in job_elements:
-			cols.append(extract_company_indeed(element))
-		extracted_info.append(companies)
+			companies.append(extract_company_indeed(element)) 
+		extracted_info.append(companies) 
 
 	if 'links' in desired_result:
 		links = []
@@ -94,6 +126,7 @@ def extract_job_information_indeed(jobs, desired_result):
 			links.append(extract_link_indeed(element))
 		extracted_info.append(links)
 
+
 	if 'date_listed' in desired_result:
 		dates = []
 		cols.append('date_listed')
@@ -101,16 +134,27 @@ def extract_job_information_indeed(jobs, desired_result):
 			dates.append(extract_date_indeed(element))
 		extracted_info.append(dates)
 
+
 	jobs_list = {}
-	for j in range(len(cols)):
-		jobs_list[cols[j]] = extracted_info[j]
+	# print(cols)
+	# # print(range(len(extracted_info)))
+	# # print(jobs_list) 
+	# print(len(extracted_info[0]))
+	# quit()
+	for j in range(len(cols)): 
+		jobs_list[cols[j]] = extracted_info[j] 
+
 
 	num_listings = len(extracted_info[0])
 	return jobs_list, num_listings
 
-
-
-
+def load_indeed_jobs_div(job_title, location):
+	args = {'q': job_title, 'l': location, 'fromage':'last', 'sort': 'date'}
+	url = ('http://www.indeed.co.uk/jobs?' + urllib.parse.urlencode(args))
+	page = requests.get(url)
+	soup = BeautifulSoup(page.content, "lxml")
+	jobs = soup.find(id="resultsCol")
+	return jobs
 
 def extract_job_title_indeed(element):
 	title = element.find('h2', class_='title').text.strip()
@@ -118,12 +162,12 @@ def extract_job_title_indeed(element):
 
 
 def extract_company_indeed(element):
-	company = element.find('span', class_='company'),text.strip()
+	company = element.find('span', class_='company').text.strip()
 	return company
 
 
 def extract_link_indeed(element):
-	link = 'www.indeed.co.uk/' + element.find('a')['href']
+	link = 'www.indeed.co.uk' + element.find('a')['href']
 	return link 
 
 
@@ -132,4 +176,31 @@ def extract_date_indeed(element):
 	return date
 
 
+if __name__ == '__main__':
+	intro = """\
+	###############################
+	#                             # 
+	#      INDEED JOB SCRAPER     #   
+	#                             #	 
+	###############################
+	"""
+	print(intro)
+
+	job_title = input("Enter a job title to search listings.\n> ")
+
+	location = input("Enter a location.\n> ")
+
+	try:
+		output = int(input("""
+Select a method to retrieve results.\n
+Type 1 for Email.\n
+Type 2 for Excel.\n> """))
+	except ValueError:
+		output = int(input("""
+There has been an error.\n
+Please enter the number 1 for results in an email format\n
+or 2 for an Excel output.\n> """))
+		
+	desired_result = ['titles', 'companies', 'links', 'date_listed']
+	search_job_listings(job_title, location, desired_result, output)
 
